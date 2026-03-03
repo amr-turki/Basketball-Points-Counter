6d35708575f6db83ddb42a5d348d59d7bcf7c7e4
Amr Turki <amrturki79@gmail.com>
2026-03-03 09:58:10 +0200

    Initial commit - Basketball Points Counter

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BasketballPointsCounter());
+}
+
+class BasketballPointsCounter extends StatelessWidget {
+  const BasketballPointsCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold());
+  }
+}
